8f395e0447b864fbe9d497f15d047d6bb6b99e90
kahuz <67911543+kahuz@users.noreply.github.com>
2021-11-02 22:47:46 +0900

    added object rotate function & icon resource
    # author : kahuz
    # descript : 3D object 회전 기능 추가와 아이콘 리소스 추가
    ## 3D object 회전 기능
     - imgui로 생성한 버튼 클릭 시 마우스 위치 변화에 따른 회전 적용
    ## 아이콘 리소스 추가
     - 3D object 제어를 위한 아이콘 바에 사용할 무료 라이센스 아이콘들 추가

--- src/main.cpp
+++ src/main.cpp
@@ -62,7 +62,7 @@ using namespace gl;
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
 
-static void glfw_error_callback(int error, const char *description)
+static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
@@ -171,7 +171,7 @@ void DrawMenuBar()
         if (ImGui::BeginMenu("File"))
         {
             DrawFileMenuBar();
-            
+
             ImGui::EndMenu();
         }
         if (ImGui::BeginMenu("Edit"))
@@ -188,7 +188,7 @@ void DrawMenuBar()
     }
 }
 
-int main(int, char **)
+int main(int, char**)
 {
     // Setup window
     glfwSetErrorCallback(glfw_error_callback);
@@ -196,14 +196,12 @@ int main(int, char **)
         return 1;
 
     // GL 3.0 + GLSL 130
-    const char *glsl_version = "#version 130";
+    const char* glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 
     // Create window with graphics context
-    GLFWwindow *window = glfwCreateWindow(640, 640, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 640, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
     if (window == NULL)
         return 1;
     glfwMakeContextCurrent(window);
@@ -220,43 +218,23 @@ int main(int, char **)
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO &io = ImGui::GetIO();
-    (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
     // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
+    ImGui::StyleColorsDark(); //ImGui::StyleColorsClassic();
 
     // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
-
     const char vShaderStr[] =
         "#version 130 \n"
         "attribute vec4 vPosition;    \n"
         "uniform mat4 uProjection;    \n"
-        "uniform mat4 uTransform;    \n"
+        "uniform mat4 uModel;	      \n"
+        "uniform mat4 uView;	      \n"
         "void main()                  \n"
         "{                            \n"
-        "   gl_Position = uProjection * uTransform * vPosition; \n"
-        //"   gl_Position = vPosition;  \n"
+        "   gl_Position = uProjection * uView * uModel  * vPosition; \n"
         "}                            \n";
 
     const char fShaderStr[] =
@@ -272,12 +250,13 @@ int main(int, char **)
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     //init GLSL Program
-    GLShader *my_gl = new GLShader(vShaderStr, fShaderStr);
+    GLShader* my_gl = new GLShader(vShaderStr, fShaderStr);
 
     //set vertex Attribute vPosition
     my_gl->SetGLAttribLocation(GL_VERTEX_SHADER, "vPosition");
     my_gl->SetGLUniformLocation(GL_VERTEX_SHADER, "uProjection");
-    my_gl->SetGLUniformLocation(GL_VERTEX_SHADER, "uTransform");
+    my_gl->SetGLUniformLocation(GL_VERTEX_SHADER, "uModel");
+    my_gl->SetGLUniformLocation(GL_VERTEX_SHADER, "uView");
 
     static float f = 0.0f;
     static int counter = 0;
@@ -292,7 +271,6 @@ int main(int, char **)
         // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         glfwPollEvents();
 
-
         // Start the Dear ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
@@ -300,6 +278,14 @@ int main(int, char **)
 
         DrawMenuBar();
 
+        static int x = 0, y = 0, frame_num = 0;
+        static float angle = 90, cur_x_pos = 0, prev_x_pos = 0, cur_y_pos = 0, prev_y_pos = 0;
+        static float angle_x = 0.0f, angle_y = 0.0f;
+        static bool active_rotate = false;
+        static std::string rotate_btn_str = "Disable Rotate";
+        int mouse_state = 0; // 0 release, 1 press
+
+        ImGuiIO& io = ImGui::GetIO();
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
         {
             ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
@@ -309,14 +295,50 @@ int main(int, char **)
             ImGui::Checkbox("Another Window", &show_another_window);
 
             ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
             if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
                 counter++;
+
+            if (ImGui::Button(rotate_btn_str.c_str()))
+            {
+                active_rotate = !active_rotate;
+
+                if (active_rotate)
+                {
+                    rotate_btn_str = "Enable Rotate";
+                }
+                else
+                {
+                    rotate_btn_str = "Disable Rotate";
+                }
+            }
             ImGui::SameLine();
             ImGui::Text("counter = %d", counter);
 
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
+            for (int i = 0; i < IM_ARRAYSIZE(io.MouseDown); i++)
+            {
+                if (ImGui::IsMouseDown(i))
+                {
+                    mouse_state = 1;
+                }
+            }
+
+            for (int i = 0; i < IM_ARRAYSIZE(io.MouseDown); i++)
+            {
+                if (ImGui::IsMouseReleased(i))
+                {
+                    mouse_state = 0;
+                }
+            }
+
+            std::string mouse_state_str = mouse_state ? "Clicked" : "Released";
+            ImGui::Text("Mouse state %s", mouse_state_str.c_str());
+            ImGui::Text("angle x %f", angle_x);
+            ImGui::Text("angle y %f", angle_y);
+
             ImGui::End();
         }
 
@@ -334,47 +356,114 @@ int main(int, char **)
         ImGui::Render();
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
-        static int x = 0, y = 0, frame_num = 0;
-
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
 
         if (load_model)
         {
-            GLfloat project_mat[16] =
+            frame_num++;
+
+            if (frame_num % 33 == 0)
             {
-                0.5f, 0.0f, 0.0f, 0.0f,
-                0.0f, 0.5f, 0.0f, 0.0f,
-                0.0f, 0.0f, 0.5f, 0.0f,
+                angle++;
+            }
+            GLfloat mProjeMat[16] =
+            {
+                0.3f, 0.0f, 0.0f, 0.0f,
+                0.0f, 0.3f, 0.0f, 0.0f,
+                0.0f, 0.0f, 0.3f, 0.0f,
                 0.0f, 0.0f, 0.0f, 1.0f
             };
 
-            GLfloat obj_rotate[16] =
+            GLfloat mViewMat[16] =
             {
                 1.0f, 0.0f, 0.0f, 0.0f,
                 0.0f, 1.0f, 0.0f, 0.0f,
                 0.0f, 0.0f, 1.0, 0.0f,
                 0.0f, 0.0f, 0.0f, 1.0f
             };
+
+            GLfloat mModelMat[16] =
+            {
+                1.0f, 0.0f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 1.0, 0.0f,
+                0.0f, 0.0f, 0.0f, 1.0f
+            };
+
+            if (mouse_state == 1 && active_rotate)
+            {
+                cur_x_pos = io.MousePos.x;
+                cur_y_pos = io.MousePos.y;
+
+                if (cur_x_pos != prev_x_pos)
+                {
+                    angle_x += std::fmod((cur_x_pos - prev_x_pos), 360.0f) / 20;
+
+                    if (angle_x > 360.0f || angle_x < -360.0f)
+                    {
+                        angle_x = 0.0f;
+                    }
+                }
+                if (cur_y_pos != prev_y_pos)
+                {
+                    angle_y += std::fmod((cur_y_pos - prev_y_pos), 360.0f) / 20;
+
+                    if (angle_y > 360.0f || angle_y < -360.0f)
+                    {
+                        angle_y = 0.0f;
+                    }
+                }
+            }
+
+            float x_s = sinf(angle_y);
+            float x_c = cosf(angle_y);
+
+            float y_s = sinf(angle_x);
+            float y_c = cosf(angle_x);
+
+            GLfloat mRotate_X_Mat[16] =
+            {
+                1.f, 0.f, 0.f, 0.f,
+                0.f,   x_c,   x_s, 0.f,
+                0.f,  -x_s,   x_c, 0.f,
+                0.f, 0.f, 0.f, 1.f
+            };
+
+            GLfloat mRotate_Y_Mat[16] = {
+                   y_c, 0.f,  -y_s, 0.f,
+                 0.f, 1.f, 0.f, 0.f,
+                   y_s, 0.f,   y_c, 0.f,
+                 0.f, 0.f, 0.f, 1.f
+            };
+
+            
+
             //linmath test
             glViewport(0, 0, display_w, display_h);
 
             glUseProgram(my_gl->program);
 
-            glUniformMatrix4fv(my_gl->vert_member.at("uProjection"), 1, GL_FALSE, project_mat);
-            glUniformMatrix4fv(my_gl->vert_member.at("uTransform"), 1, GL_FALSE, obj_rotate);
+            glUniformMatrix4fv(my_gl->vert_member.at("uProjection"), 1, GL_FALSE, mProjeMat);
+            //glUniformMatrix4fv(my_gl->vert_member.at("uView"), 1, GL_FALSE, mViewMat);
+            glUniformMatrix4fv(my_gl->vert_member.at("uView"), 1, GL_FALSE, mRotate_X_Mat);
+
+            //glUniformMatrix4fv(my_gl->vert_member.at("uModel"), 1, GL_FALSE, mModelMat);
+            glUniformMatrix4fv(my_gl->vert_member.at("uModel"), 1, GL_FALSE, mRotate_Y_Mat);
 
             glEnableVertexAttribArray(my_gl->vert_member.at("vPosition"));
             glVertexAttribPointer(my_gl->vert_member.at("vPosition"), 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), &model.positions[0]);
-            
+
             glLineWidth(3);
 
             glDrawElements(GL_LINES, model.v_faces.size(), GL_UNSIGNED_INT, &model.v_faces[0]);
 
+            prev_x_pos = cur_x_pos;
+            prev_y_pos = cur_y_pos;
         }
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
-    }              
+    }
     // Cleanup
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
